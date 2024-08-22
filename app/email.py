66e12ae489102f5c9ba3676ed05f30439b193fe0
Enzo Pelegrini <113308665+epelegrini@users.noreply.github.com>
2024-08-22 20:55:21 -0300

    Update email.py

--- app/email.py
+++ app/email.py
@@ -1,20 +1,40 @@
-import os
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+from threading import Thread
+from flask import current_app, render_template
+from flask_mail import Message
+from . import mail
 
-def send_email(subject, recipient, body):
-    api_key = os.environ.get('SENDGRID_API_KEY')
-    if not api_key:
-        raise ValueError("SENDGRID_API_KEY not found in environment variables")
-    sg = SendGridAPIClient(api_key)
-    from_email = 'flaskaulas@zohomail.com' 
-    message = Mail(
-        from_email=from_email,
-        to_emails=recipient,
-        subject=subject,
-        html_content=body)
-    try:
-        response = sg.send(message)
-        print(f"Email sent to {recipient}. Status code: {response.status_code}")
-    except Exception as e:
-        print(f"Error sending email: {e}")
+import requests
+from datetime import datetime
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_email(to, subject, template, **kwargs):
+    app = current_app._get_current_object()
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])    
+    msg.html = render_template(template + '.html', **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
+
+def send_simple_message(to, subject, newUser):
+    app = current_app._get_current_object()
+    print('Enviando mensagem (POST)...', flush=True)
+    print('URL: ' + str(app.config['API_URL']), flush=True)
+    print('api: ' + str(app.config['API_KEY']), flush=True)
+    print('from: ' + str(app.config['API_FROM']), flush=True)
+    print('to: ' + str(to), flush=True)
+    print('subject: ' + str(app.config['FLASKY_MAIL_SUBJECT_PREFIX']) + ' ' + subject, flush=True)
+    print('text: ' + "Novo usuário cadastrado: " + newUser, flush=True)
+
+    resposta = requests.post(app.config['API_URL'], 
+                             auth=("api", app.config['API_KEY']), data={"from": app.config['API_FROM'], 
+                                                                        "to": to, 
+                                                                        "subject": app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject, 
+                                                                        "text": "Novo usuário cadastrado: " + newUser})
+        
+    print('Enviando mensagem (Resposta)...' + str(resposta) + ' - ' + datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), flush=True)
+    return resposta
