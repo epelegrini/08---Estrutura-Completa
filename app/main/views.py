6cfb327c70c0c8fd03d7b707e981fdc3bda4b2dc
Fabio Teixeira <53839583+fabio-teixeira-ifsp@users.noreply.github.com>
2024-08-22 15:42:56 -0300

    Importação do send_simple_message

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, session, redirect, url_for, current_app
 from .. import db
 from ..models import User
-from ..email import send_email
+from ..email import send_email, send_simple_message
 from . import main
 from .forms import NameForm
 
